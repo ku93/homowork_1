1c8fa5dbf6ccbdfd53d8955333de43ff28945ec2
Леонид Ткачев <tkachev1993adg@yandex.ru>
2024-07-30 20:16:48 +0300

    src/masks.py

--- src/widget.py
+++ src/widget.py
@@ -1,37 +1,42 @@
 from masks import get_mask_account, get_mask_card_number
 
-inp=input("Введите Ваши реквезиты: ")
-name_card=""
-number_card= ""
+inp = input("Введите Ваши реквезиты: ")
+name_card = ""
+number_card = ""
 for i in inp:
     if i.isalpha():
-        name_card+=i
-    elif i==" ":
+        name_card += i
+    elif i == " ":
         name_card += i
     elif i.isdigit():
-        number_card+=i
+        number_card += i
 
-def mask_account_card(inp :str) ->str:
-    """"Функция которая умеет обрабатывать информацию как о картах, так и о счетах"""
+
+def mask_account_card(inp: str) -> str:
+    """ "Функция которая умеет обрабатывать информацию как о картах, так и о счетах"""
     if "счет" in inp.lower():
-        account=number_card
-        rechnung=get_mask_account(account)
-        new_rechnung=name_card.title()+" "+rechnung
+        account = number_card
+        rechnung = get_mask_account(account)
+        new_rechnung = name_card.title() + " " + rechnung
         return new_rechnung
     else:
-        card_number=number_card
-        card=get_mask_card_number(card_number)
-        new_card=name_card.title()+": "+card
+        card_number = number_card
+        card = get_mask_card_number(card_number)
+        new_card = name_card.title() + ": " + card
         return new_card
+
+
 print(mask_account_card(inp))
 
-data_inp=input("Введите дату: ")
-def get_data(data_inp: str) ->str:
+data_inp = input("Введите дату: ")
+
+
+def get_data(data_inp: str) -> str:
     """Функция, которая принимает и преобразует формат даты"""
-    data_sp=data_inp.split("T")
-    data=data_sp[0].split("-")
+    data_sp = data_inp.split("T")
+    data = data_sp[0].split("-")
     new_data = ".".join(reversed(data))
     return new_data
+
+
 print(get_data(data_inp))
-
-
