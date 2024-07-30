4c7b1e7aa8fa95ee190925554038f205e9d3b742
Леонид Ткачев <tkachev1993adg@yandex.ru>
2024-07-30 13:49:17 +0300

    masks.py

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция, которая создает маску карты"""
+    masked_card = card_number[:4] + " " + card_number[4:6] + "** ****" + " " + card_number[-4:]
+    return masked_card
+
+def get_mask_account(account: str) -> str:
+    """Функция, которая создает маску счета"""
+    stars = 2*"*"
+    mask_account = stars + account[2:]
+    return mask_account
