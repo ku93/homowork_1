40036d4e184e615d53c387a2b2c5192e7245c13f
tkach <tkachev1993adg@yandex.ru>
2024-08-11 17:45:02 +0300

    fask_3

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,40 +14,50 @@ def payments():
 @pytest.fixture
 def transactions():
     return [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {"name": "USD", "code": "USD"},
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
+    {
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount":
+              {
+              "amount": "9824.07",
+              "currency":
+                  {
+                  "name": "USD",
+                  "code": "USD"}
+              },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+    },
+    {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "79114.93",
+                  "currency": {
+                      "name": "USD",
+                      "code": "USD"
+                  }
+              },
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+    },
+    {
+        "id": 142264000,
+        "state": "EXECUTED",
+        "date": "2010-06-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "714.00",
+            "currency": {
+                "name": "EUR",
+                "code": "EUR"
+            }
         },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {"name": "USD", "code": "USD"},
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188",
-        },
-        {
-            "id": 791923212,
-            "state": "EXECUTED",
-            "date": "2019-06-08T16:32:22.850589",
-            "operationAmount": {
-                "amount": "10451.88",
-                "currency": {"name": "EUR", "code": "EUR"},
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        },
-    ]
+        "description": "Перевод с карты на карту",
+        "from": "Счет 19708645243227258000",
+        "to": "Счет 75651667383060284111"
+    }
+]
